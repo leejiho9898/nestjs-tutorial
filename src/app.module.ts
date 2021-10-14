c9503f1c215567a59f414b1ab65fda3d6f76fa83
이지호 <cctv1998@naver.com>
2021-10-14 18:11:21 +0900

    Test : Board => board

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BoardsModule } from './Boards/boards.module';
-
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
   imports: [BoardsModule],
-
 }) //모듈의 허브 라고 생각하면됨 다른 모듈들 모아주는기능
 export class AppModule {}
