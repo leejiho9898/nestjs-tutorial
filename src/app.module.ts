a1e31453f2268b8266b9aa82ccb9a388b1b69eee
이지호 <cctv1998@naver.com>
2021-10-19 15:10:59 +0900

    Feat : board repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
 }) //모듈의 허브 라고 생각하면됨 다른 모듈들 모아주는기능
 export class AppModule {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
+  imports:[
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardsController], //들어오는 요청을 처리하고 클라이언트에 응답을 반환함
   providers: [BoardsService] //서비스를 어떤걸 쓸지 결정함
 })
