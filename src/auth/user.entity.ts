e3d0b0d7dc66a963529806c7a1e6dfdc8e212151
이지호 <cctv1998@naver.com>
2021-10-26 20:29:56 +0900

    Feat : auth entity

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+
+@Entity()
+@Unique(['username'])
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+}
