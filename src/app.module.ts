78658c73fdfe0b4640a28da90f49d2134c17da96
이지호 <cctv1998@naver.com>
2021-10-26 20:29:23 +0900

    Feat : auth module 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
 }) //모듈의 허브 라고 생각하면됨 다른 모듈들 모아주는기능
 export class AppModule {}
